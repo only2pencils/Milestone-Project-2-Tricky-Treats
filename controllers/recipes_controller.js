46e7dbad818d6a8ec748847f8b3f199f959d4180
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-26 19:31:25 -0400

    recipes controller complete

---
+++ controllers/recipes_controller.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const recipe = express.Router();
+const Recipe = require("../models/recipe.js");
+const recipeSeedData = require("../models/recipe_seed.js");
+
+//index//
+recipe.get("/", (req, res) => {
+  Recipe.find()
+    .populate("treats")
+    .then((foundRecipes) => {
+      res.send(foundRecipes);
+    });
+});
+
+//show//
+recipe.get("/:id", (req, res) => {
+  Recipe.findById(req.params.id)
+    .populate({
+      path: "recipes",
+      options: { limit: 3 },
+    })
+    .lean()
+    .then((foundRecipe) => {
+      res.render("recipeShow", {
+        recipe: foundRecipe,
+      });
+    });
+});
+
+//delete//
+recipe.delete("/:id", (req, res) => {
+  Recipe.findByIdAndDelete(req.params.id).then((deletedRecipe) => {
+    res.status(303).redirect("/treats");
+  });
+});
+
+module.exports = recipe;
