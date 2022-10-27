9fb4252f5549093d3927fde31639db3aec5f2a2d
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-26 20:25:52 -0400

    recipe.js file complete

---
+++ models/recipe.js
@@ -0,0 +1,46 @@
+//dependencies//
+const mongoose = require("mongoose");
+const Treat = require("./treat");
+const { Schema } = mongoose;
+
+//schema//
+const recipeSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      enum: [
+        "Dracula",
+        "Frankenstein",
+        "PumpkinHead",
+        "Witch",
+        "Oogie Boogie",
+        "Casper",
+      ],
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    bio: String,
+  },
+  { toJSON: { virtuals: true } }
+);
+
+//virtuals//
+recipeSchema.virtual("brebas", {
+  ref: "Treat",
+  localField: "_id",
+  foreignField: "recipe",
+});
+
+//hooks//
+recipeSchema.post("findOneAndDelete", function () {
+  Treat.deleteMany({ recipe: this._conditions._id }).then((deleteStatus) => {
+    console.log(deleteStatus);
+  });
+});
+
+//model and export//
+const Recipe = mongoose.model("Recipe", recipeSchema);
+module.exports = Recipe;
