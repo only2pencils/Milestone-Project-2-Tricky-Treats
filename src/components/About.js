91cca2ce87d7a320efe6f74650b86d3a15bfc0b6
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-28 02:14:10 -0400

    added About Us page

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <div className="aboutpage">
+      <div className="aboutHeader">
+        <h1>About Us</h1>
+      </div>
+      <p>We are FULLSTACK ROCKSTARS!!!</p>
+      <img src="" alt="" />
+    </div>
+  );
+}
