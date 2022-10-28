73cbdfe1287b936d02f725f6ec0120af32a43f72
only2pencils <89320439+only2pencils@users.noreply.github.com>
2022-10-28 19:59:34 -0400

    fixed syntax errors in controller

--- controllers/trickytreats_controller.js.js
+++ controllers/trickytreats_controller.js.js
@@ -1,11 +1,11 @@
 const express = require("express");
-const treats = express.Router();
-const Treat = require("../models/treat.js");
+const trickytreats = express.Router();
+const TrickyTreat = require("../models/trickytreat.js");
 const Recipe = require("../models/recipe.js");
-const treatSeedData = require("../models/seed.js");
+const trickytreatSeedData = require("../models/seed.js");
 
 //INDEX//
-treats.get("/", async (req, res) => {
+trickytreats.get("/", async (req, res) => {
   const foundRecipes = await Recipe.find().lean();
   const foundTreats = await Treat.find().limit(2).lean();
   console.log(foundTreats);
@@ -17,7 +17,7 @@ treats.get("/", async (req, res) => {
 });
 
 //NEW//
-treats.get("/new", (req, res) => {
+trickytreats.get("/new", (req, res) => {
   Recipe.find().then((foundRecipes) => {
     res.render("new", {
       recipes: foundRecipes,
@@ -26,12 +26,12 @@ treats.get("/new", (req, res) => {
 });
 
 //SHOW//
-treats.get("/:id", (req, res) => {
-  Treat.findById(req.params.id)
+trickytreats.get("/:id", (req, res) => {
+  TrickyTreat.findById(req.params.id)
     .populate("recipe")
-    .then((foundTreat) => {
+    .then((foundTrickyTreat) => {
       res.render("show", {
-        treat: foundTreat,
+        trickytreat: foundTrickyTreat,
       });
     })
     .catch((err) => {
@@ -40,7 +40,7 @@ treats.get("/:id", (req, res) => {
 });
 
 //EDIT//
-treats.get("/:id/edit", (req, res) => {
+trickytreats.get("/:id/edit", (req, res) => {
   Recipe.find().then((foundRecipes) => {
     Treat.findById(req.params.id).then((foundTreat) => {
       res.render("edit", {
@@ -52,22 +52,22 @@ treats.get("/:id/edit", (req, res) => {
 });
 
 //UPDATE//
-treats.put("/:id", (req, res) => {
+trickytreats.put("/:id", (req, res) => {
   if (req.body.hasGhost === "on") {
     req.body.hasGhost = true;
   } else {
     req.body.hasGhost = false;
   }
-  Treat.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
-    (updatedTreat) => {
-      console.log(updatedTreat);
-      res.redirect(`/treats/${req.params.id}`);
+  TrickyTreat.findByIdAndUpdate(req.params.id, req.body, { new: true }).then(
+    (updatedTrickyTreat) => {
+      console.log(updatedTrickyTreat);
+      res.redirect(`/trickytreats/${req.params.id}`);
     }
   );
 });
 
 //CREATE//
-treats.post("/", (req, res) => {
+trickytreats.post("/", (req, res) => {
   if (!req.body.image) {
     req.body.image = undefined;
   }
@@ -81,17 +81,17 @@ treats.post("/", (req, res) => {
 });
 
 //DELETE//
-treats.delete("/:id", (req, res) => {
+trickytreats.delete("/:id", (req, res) => {
   Treat.findByIdAndDelete(req.params.id).then((deletedTreat) => {
     res.status(303).redirect("/treats");
   });
 });
 
 //SEED ROUTE//
-treats.get("/data/seed", (req, res) => {
-  Treat.insertMany(treatSeedData).then((createTreats) => {
-    res.redirect("/treats");
+trickytreats.get("/data/seed", (req, res) => {
+  TrickyTreat.insertMany(trickytreatSeedData).then((createTrickyTreats) => {
+    res.redirect("/trickytreats");
   });
 });
 
-module.exports = treats;
+module.exports = trickytreats;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
-export default function Home() {
-  return (
-    <div className="homepage">
-      <h1>Come and try a Treat, I DARE you.</h1>
+// export default function Home() {
+//   return (
+//     <div className="homepage">
+//       <h1>Come and try a Treat, I DARE you.</h1>
 
-      <h1>Check out our wide array of Treats</h1>
-    </div>
-  );
-}
+//       <h1>Check out our wide array of Treats</h1>
+//     </div>
+//   );
+// }
